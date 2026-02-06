55cff3aa9624dbcc2ce7e776b9790a7c3adbaae2
User <user@example.com>
2026-02-06 22:34:59 +0700

    Fix test configuration by adding Application.class to @SpringBootTest

--- src/test/java/com/example/jwt_learn/JwtLearnApplicationTests.java
+++ src/test/java/com/example/jwt_learn/JwtLearnApplicationTests.java
@@ -2,8 +2,9 @@ package com.example.jwt_learn;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import com.example.app.Application;
 
-@SpringBootTest
+@SpringBootTest(classes = Application.class)
 class JwtLearnApplicationTests {
 
 	@Test
